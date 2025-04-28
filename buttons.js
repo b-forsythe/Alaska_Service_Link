fb32ca6d04ae527fc8a07dc5831221dcc4892d33
b-forsythe <business.brandonforsythe@gmail.com>
2025-04-28 14:57:49 -0400

    create and test push

---
+++ buttons.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Helper function to handle clicks
+    function handleButtonClick(id, action) {
+        const button = document.getElementById(id);
+        if (button) {
+            button.addEventListener("click", action);
+        }
+    }
+
+    // Event listeners for each button
+    handleButtonClick("how_work", function() {
+        console.log("HOW IT WORKS!");
+    });
+
+    handleButtonClick("login", function() {
+        window.location.href = "/login";
+    });
+
+    handleButtonClick("contractor_login", function() {
+        console.log("Logging in as Contractor");
+    });
+
+    handleButtonClick("client_login", function() {
+        console.log("Logging in as User");
+    });
+
+    handleButtonClick("admin_login", function() {
+        console.log("Logging in as Admin");
+    });
+
+    handleButtonClick("home", function() {
+        window.location.href="/index.html";
+    });
+
+});
