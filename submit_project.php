2d023c879787d27582fa9be105e3f351a21b74b5
b-forsythe <business.brandonforsythe@gmail.com>
2025-05-03 12:22:10 -0400

    test push

--- submit_project.php
+++ submit_project.php
@@ -12,7 +12,7 @@ $dbname = "ufzauemy_project_submissions";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
-if ($conn->connect_error) {
+if ($conn->connect_error); {
     die("Connection failed: " . $conn->connect_error);
 }
 
